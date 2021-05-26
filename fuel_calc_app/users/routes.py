3938e9cd350ae6da51508fcd14912f0c1075f02f
mbegma <mbegma@dataplus.ru>
2021-05-26 19:04:51 +0300

    add some routes

--- fuel_calc_app/users/routes.py
+++ fuel_calc_app/users/routes.py
@@ -8,10 +8,11 @@
 # Description: 
 # Copyright: (c) Дата+, 2021
 # -----------------------------------------------------
+from datetime import datetime
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_user, current_user, login_required, logout_user
-from .forms import RegisterForm, LoginForm
-from fuel_calc_app.models import User
+from .forms import RegisterForm, LoginForm, AddCarForm, EditProfileForm
+from fuel_calc_app.models import User, Car
 from fuel_calc_app import db
 
 # Blueprint configuration
@@ -21,15 +22,38 @@ user_bp = Blueprint(
     static_folder='static'
 )
 
+# @user_bp.route('/user/<username>')
+# @login_required
+# def user(username):
+#     u = User.query.filter_by(username=username).first_or_404()
+#     cars = [
+#         {'car_name': 'car name #1', 'car_model': 'Car model #1'},
+#         {'car_name': 'car name #2', 'car_model': 'Car model #2'},
+#     ]
+#     return render_template('user.html', user=user, posts=cars)
 
-@user_bp.route('/profile')
+
+@user_bp.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@user_bp.route('/profile/<username>')
 @login_required
-def profile():
+def profile(username):
+    u = User.query.filter_by(user_name=username).first_or_404()
+    cars = [
+        {'car_name': 'car name #1', 'car_model': 'Car model #1'},
+        {'car_name': 'car name #2', 'car_model': 'Car model #2'}
+    ]
     return render_template(
         'profile.jinja2',
         title='Profile Page',
         subtitle='user profile',
-        template='users-template'
+        template='users-template',
+        user=u, cars=cars
     )
 
 
@@ -48,7 +72,7 @@ def register():
         db.session.commit()
         login_user(new_user)
         flash('Thanks for registering, {}!'.format(new_user.email))
-        return redirect(url_for('user_bp.profile'))
+        return redirect(url_for('user_bp.profile', username=new_user.user_name))
     return render_template(
         'register.jinja2',
         title='Register Page',
@@ -62,7 +86,7 @@ def login():
     # If the User is already logged in, don't allow them to try to log in again
     if current_user.is_authenticated:
         flash('Already logged in!  Redirecting to your User Profile page...')
-        return redirect(url_for('user_bp.profile'))
+        return redirect(url_for('user_bp.profile', username=current_user.user_name))
 
     form = LoginForm()
 
@@ -76,7 +100,7 @@ def login():
                 db.session.commit()
                 login_user(user, remember=form.remember_me.data)
                 flash('Thanks for logging in, {}!'.format(current_user.email))
-                return redirect(url_for('user_bp.profile'))
+                return redirect(url_for('user_bp.profile', username=user.user_name))
 
         flash('ERROR! Incorrect login credentials.')
     return render_template(
@@ -90,11 +114,63 @@ def login():
 @user_bp.route('/logout')
 @login_required
 def logout():
-    user = current_user
-    user.authenticated = False
-    db.session.add(user)
+    u = current_user
+    u.authenticated = False
+    db.session.add(u)
     db.session.commit()
     logout_user()
     flash('Goodbye!')
     return redirect(url_for('home_bp.home'))
+
+
+@user_bp.route('/add_car', methods=['GET', 'POST'])
+@login_required
+def add_car():
+    form = AddCarForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        new_car = Car(car_name=form.car_name.data,
+                      car_model=form.car_model.data,
+                      car_year=form.car_year.data,
+                      is_default_car=form.car_default.data,
+                      user_id=current_user.id)
+        db.session.add(new_car)
+        db.session.commit()
+        flash('Автомобиль {} добавлен в гараж !'.format(new_car.name))
+        return redirect(url_for('user_bp.profile', username=current_user.user_name))
+    return render_template(
+        'add_car.jinja2',
+        title='Add Car Page',
+        subtitle='add car',
+        template='users-template',
+        form=form)
+
+
+@user_bp.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.user_name = form.user_name.data
+        current_user.email = form.email.data
+        current_user.about_me = form.about_me.data
+        current_user.location = form.location.data
+        current_user.gender = form.gender.data
+
+        # return form.dt.data.strftime('%Y-%m-%d')
+        current_user.birthday = form.birthday.data  # .strftime('%Y-%m-%d')
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('user_bp.profile', username=current_user.user_name))
+    elif request.method == 'GET':
+        form.user_name.data = current_user.user_name
+        form.email.data = current_user.email
+        form.about_me.data = current_user.about_me
+        form.location.data = current_user.location
+        form.gender.data = current_user.gender
+        form.birthday = current_user.birthday
+    return render_template(
+        'edit_profile.jinja2',
+        title='Edit User Profile',
+        template='users-template',
+        form=form)
 
